e597b6611655ad010f8328096c4516ee394a4eb5
danielstevenescobar <125221567+danielstevenescobar@users.noreply.github.com>
2023-02-19 21:19:11 -0300

    Add files via upload

---
+++ Bucles wile Main.java
@@ -0,0 +1,13 @@
+package Ejercicio_practica_open.bootcamp;
+
+ public class Main {
+
+    public static void main(String[] args) {
+        int contador = 20;
+
+        while (contador > 0) {
+            System.out.println(contador);
+            contador = contador - 2;
+        }
+    }
+}
