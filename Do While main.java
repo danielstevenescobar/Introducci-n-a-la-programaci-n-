f82f7e05a7660f4c767fa2eadaad44d39395e308
danielstevenescobar <125221567+danielstevenescobar@users.noreply.github.com>
2023-02-19 21:34:20 -0300

    Add files via upload

---
+++ Do While main.java
@@ -0,0 +1,13 @@
+package Ejercicio_practica_open.bootcamp;
+
+ public class Main {
+
+     public static void main(String[] args) {
+         int contador = 15;
+
+         do {
+             System.out.println(contador);
+             contador = contador - 2;
+         } while (contador > 3);
+     }
+ }
