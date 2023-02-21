c2b2cbbbc1945025be89c1221098cd6981474987
danielstevenescobar <125221567+danielstevenescobar@users.noreply.github.com>
2023-02-20 21:44:23 -0300

    Add files via upload

---
+++ Ejercicio Bucle for complejo.java
@@ -0,0 +1,11 @@
+package Ejercicio_ejemplos_open.bootcamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int contador = 1; contador <= 17; contador = contador + 2 ){
+            System.out.println(contador);
+        }
+
+    }
+}
